db602ad5880cdde309c681922034d01b2585f4a5
Watanabe Hidetoshi <hide.watanabe@socan.co.jp>
2023-05-23 13:04:38 +0900

    最新状態にアップデート

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,95 @@
-from django.shortcuts import render
+#cording:utf-8
+#認証を行うメゾットをlogin_requiredからimport
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render,get_object_or_404,redirect
+from django.utils import timezone
+from .models import Post,Comment
+from .forms import PostForm,CommentForm
+#↑　from .form import PostFormはカレントディレクトリ内にあるform.pyからimportするという意味
+#ここで、"."は"/"の意味。
 
-# Create your views here.
+def post_list(request):
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request,'blog/post_list.html',{'posts':posts})
+
+@login_required  #login_required：ログイン後に操作ができる関数（以降同様）
+#新規記事投稿
+def post_new(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)      
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            #post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail',pk=post.pk)
+    else:
+        form = PostForm()
+    return render(request, 'blog/post_edit.html',{'form': form})
+
+@login_required
+#記事編集
+def post_edit(request,pk):
+    post = get_object_or_404(Post,pk=pk)
+    if request.method == "POST":
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail',pk=post.pk)
+        else:
+            return render(request, 'blog/post_edit.html',{'form':form})
+    else:
+        form = PostForm(instance=post)
+        return render(request, 'blog/post_edit.html',{'form':form})
+
+@login_required
+def post_detail(request,pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request,'blog/post_detail.html',{'post':post})
+
+@login_required
+def post_draft_list(request):
+    posts = Post.objects.filter(published_date__isnull=True).order_by('created_date')
+    return render(request, 'blog/post_draft_list.html',{'posts': posts})
+
+@login_required
+def post_publish(request,pk):
+    post = get_object_or_404(Post,pk=pk)
+    post.publish()
+    return redirect('post_detail',pk=pk)
+
+@login_required
+def post_remove(request,pk):
+    post = get_object_or_404(Post,pk=pk)
+    post.delete()
+    return redirect('post_list')
+
+def add_comment_to_post(request, pk):
+    post = get_object_or_404(Post, pk=pk)    #postにget_object(データがある場合)もしくは404（URLが見つからない場合）を入れる
+    if request.method == "POST": 
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = post
+            comment.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = CommentForm()
+    return render(request, 'blog/add_comment_to_post.html', {'form': form})
+
+#コメント承認
+@login_required
+def comment_approve(request, pk):
+    comment = get_object_or_404(Comment, pk=pk)
+    comment.approve()
+    return redirect('post_detail', pk=comment.post.pk)
+
+#コメント削除
+@login_required
+def comment_remove(request, pk):
+    comment = get_object_or_404(Comment, pk=pk)
+    comment.delete()
+    return redirect('post_detail', pk=comment.post.pk)
