50431bf582119d18864320849bc9a942040be56a
Watanabe Hidetoshi <hide.watanabe@socan.co.jp>
2023-05-30 09:58:27 +0900

    0530_0958変更

--- blog/urls.py
+++ blog/urls.py
@@ -3,7 +3,8 @@ from django.urls import path,re_path,include
 from . import views
 
 urlpatterns=[
-    path('',views.post_list,name='post_list'),
+    #path('',views.post_list,name='post_list'),
+    #path('',views.PostListView.as_view(),name='post_list'),
     path('accounts/',include('django.contrib.auth.urls')),
     path('post/<int:pk>',views.post_detail,name='post_detail'),
     path('post/new/', views.post_new, name = 'post_new'),
@@ -14,6 +15,7 @@ urlpatterns=[
     path('post/<int:pk>/comment/', views.add_comment_to_post, name='add_comment_to_post'),
     path('comment/<int:pk>/approve/', views.comment_approve, name='comment_approve'),
     path('comment/<int:pk>/remove/', views.comment_remove, name='comment_remove'),
+    path('', views.PostListView.as_view(), name='post_serch_result'),
 ]
 
 def post_edit(request,pk):
