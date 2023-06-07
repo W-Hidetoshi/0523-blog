e5b72c8d87e7d5c4f3a0ddbe21dc26e762701266
Watanabe Hidetoshi <hide.watanabe@socan.co.jp>
2023-06-07 10:39:47 +0900

    #12 0607_1038-admin.pyの追記

--- blog/admin.py
+++ blog/admin.py
@@ -1,6 +1,11 @@
 #cording:utf-8
 from django.contrib import admin
-from .models import Post,Comment
+from .models import Post,Comment,Tag
+from markdownx.admin import MarkdownxModelAdmin
 
-admin.site.register(Post)
+#管理画面にpostモデルを登録
+admin.site.register(Post,MarkdownxModelAdmin)
+#管理画面にコメントモデルを登録
 admin.site.register(Comment)
+#管理画面にtagモデルを登録
+admin.site.register(Tag)
