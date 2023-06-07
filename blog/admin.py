cb814fbb7127fe9d3592a2dcab521e117032c6d8
Watanabe Hidetoshi <hide.watanabe@socan.co.jp>
2023-06-07 10:57:15 +0900

    #12 0607_1055-markdownx部分をコメントアウト化

--- blog/admin.py
+++ blog/admin.py
@@ -1,10 +1,11 @@
 #cording:utf-8
 from django.contrib import admin
 from .models import Post,Comment,Tag
-from markdownx.admin import MarkdownxModelAdmin
+#from markdownx.admin import MarkdownxModelAdmin
 
 #管理画面にpostモデルを登録
-admin.site.register(Post,MarkdownxModelAdmin)
+admin.site.register(Post)
+#admin.site.register(Post,MarkdownxModelAdmin)
 #管理画面にコメントモデルを登録
 admin.site.register(Comment)
 #管理画面にtagモデルを登録
