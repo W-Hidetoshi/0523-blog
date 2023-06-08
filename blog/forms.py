19a43abe42417c7adb5625765c672c5d02725e4e
Watanabe Hidetoshi <hide.watanabe@socan.co.jp>
2023-06-08 11:32:04 +0900

    #15 0608-1130_新規投稿および編集フォームにカテゴライズ選択できるように実装

--- blog/forms.py
+++ blog/forms.py
@@ -2,14 +2,14 @@
 #blog>forms.py
 from django import forms
 
-from .models import Post,Comment
+from .models import Post,Comment,Category
 
 #投稿用フォーム
 class PostForm(forms.ModelForm):
     
     class Meta:
         model = Post
-        fields = ('title','text',)
+        fields = ('title','text','category')
 
 #コメント用フォーム
 class CommentForm(forms.ModelForm):
