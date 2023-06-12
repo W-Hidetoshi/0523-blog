5af0701489eadecc27aa0ef3f78c4895f876da5d
Watanabe Hidetoshi <hide.watanabe@socan.co.jp>
2023-06-12 16:37:36 +0900

    #19 0612_1635-PostListView関数実行のため、コメントアウト解除

--- blog/urls.py
+++ blog/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns=[
     #path('',views.post_list,name='post_list'),
-    #path('',views.PostListView.as_view(),name='post_list'),
+    path('',views.PostListView.as_view(),name='post_list'),
     path('accounts/',include('django.contrib.auth.urls')),
     path('post/<int:pk>',views.post_detail,name='post_detail'),
     path('post/new/', views.post_new, name = 'post_new'),
