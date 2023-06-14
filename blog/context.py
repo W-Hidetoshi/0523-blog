a6004d358f6880efcff70f511072e249a4da51ce
Watanabe Hidetoshi <hide.watanabe@socan.co.jp>
2023-06-14 11:49:35 +0900

    #19 0614_1148-context状態追加

--- blog/context.py
+++ blog/context.py
@@ -1,11 +1,12 @@
 #cording:utf-8
 #mysite/blog/context.py
 
-from .models import Category
+from .models import Category,Post#models.pyからCategoryモデルをimport 
 
 def related(request):
     context ={
-        'category_list':Category.objects.all(),
+        'category_list':Category.objects.all(),     #category_listを辞書型で定義
+        'post_list':Post.objects.all(),  #post_listを辞書型で定義
     }
     
     return context
